3df4e5048dfc0a2c79281c83757e08ff445bbe59
Luke Hsiao <lwhsiao@stanford.edu>
2017-05-15 14:42:27 -0700

    Add script to generate 100Mbps trace file for a given time

---
+++ mahimahi/gen_100Mbps.py
@@ -0,0 +1,17 @@
+
+TIME = 60  # time in seconds
+
+
+def generate_100mpbs_trace(seconds):
+    """Generate a 100Mbps trace that last for the specified time."""
+    with open('100Mbps.up', 'w') as outfile:
+        for ms_counter in range(int(seconds * 1000)):
+            if (ms_counter % 3 == 0):
+                for j in range(9):
+                    outfile.write(str(ms_counter + 1) + '\n')
+            else:
+                for j in range(8):
+                    outfile.write(str(ms_counter + 1) + '\n')
+
+
+generate_100mpbs_trace(TIME)
