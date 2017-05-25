7d14590622e21ed4f47ac893b34541476bb50c0b
Luke Hsiao <lwhsiao@stanford.edu>
2017-05-25 13:57:21 -0700

    Add arguments to bbr_experiment and remove wasted code.

--- mahimahi/bbr_experiment.py
+++ mahimahi/bbr_experiment.py
@@ -26,22 +26,11 @@ class Flags(object):
     TIME = "time"
     LOSS = "loss"
     PORT = "port"
+    CC = "congestion_control"
+    RTT = "rtt"
+    BW = "bottleneck_bandwidth"
 
     parsed_args = None
-
-
-def _read_output(pipe, q):
-    """Read output from `pipe`, when line has been read, putsline on Queue `q`."""
-    while True:
-        l = pipe.readline()
-        q.put(l)
-
-
-def _read_input(write_pipe, in_pipe_name):
-    """Read input from a pipe with name `read_pipe_name`, writing this input straight into `write_pipe`."""
-    while True:
-        with open(in_pipe_name, "r") as f:
-            write_pipe.write(f.read())
 
 
 def _generate_100mpbs_trace(seconds, filename):
@@ -66,29 +55,50 @@ def _parse_args():
     parser.add_argument('--time', dest=Flags.TIME, type=int, nargs=1,
                         help="Enter a time in seconds to run each trace.",
                         default=20)
-    parser.add_argument('--loss', dest=Flags.LOSS, type=float, nargs=argparse.REMAINDER,
-                        help="Loss rates to test.",
-                        default=[0.001, 0.01, 0.1, 1, 2, 5, 10, 15, 20, 25, 30, 40, 50])
+    parser.add_argument('--loss', dest=Flags.LOSS, type=float, nargs=1,
+                        help="Loss rate to test (%).",
+                        default=0.01)
     parser.add_argument('--port', dest=Flags.PORT, type=int, nargs=1,
                         help="Which port to use.",
                         default=5050)
+    parser.add_argument('--cc', dest=Flags.CC, type=str, nargs=1,
+                        help="Which congestion control algorithm to compare.",
+                        default="cubic")
+    parser.add_argument('--rtt', dest=Flags.RTT, type=int, nargs=1,
+                        help="Specify the RTT of the link in milliseconds.",
+                        default=100)
+    parser.add_argument('--bw', dest=Flags.BW, type=float, nargs=1,
+                        help="Specify the bottleneck bandwidth in Mbps.",
+                        default=100)
 
     Flags.parsed_args = vars(parser.parse_args())
+    # Preprocess the loss into a percentage
+    Flags.parsed_args[Flags.LOSS] = Flags.parsed_args[Flags.LOSS][0] / 100.0
     debug_print_verbose("Parse: " + str(Flags.parsed_args))
 
 
-def _run_experiment(loss, port, cong_ctrl):
+def _run_experiment(loss, port, cong_ctrl, display=True):
     """Run a single throughput experiment with the given loss rate."""
     debug_print("Running experiment [loss = " +
                 str(loss) + ", cong_ctrl = " + str(cong_ctrl) + "]")
 
-    process = subprocess.Popen(
-        ["stdbuf", "-o0", "mm-delay", "50", "mm-loss", "uplink", str(loss),
-         "mm-link", "100Mbps.up", "100Mbps.down", "--uplink-log=/tmp/bbr_log",
-         "--meter-uplink", "--once", "--", "stdbuf", "-o0", "python",
-         "client.py", str(port), str(cong_ctrl)],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE)
+    if display:
+        process = subprocess.Popen(
+            ["stdbuf", "-o0", "mm-delay", "50", "mm-loss", "uplink", str(loss),
+             "mm-link", "100Mbps.up", "100Mbps.down", "--uplink-log=/tmp/bbr_log",
+             "--meter-uplink", "--once", "--", "stdbuf", "-o0", "python",
+             "client.py", str(port), str(cong_ctrl)],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE)
+    else:
+        # If display is False, don't show it. This is used for headless operation.
+        process = subprocess.Popen(
+            ["stdbuf", "-o0", "mm-delay", "50", "mm-loss", "uplink", str(loss),
+             "mm-link", "100Mbps.up", "100Mbps.down", "--uplink-log=/tmp/bbr_log",
+             "--once", "--", "stdbuf", "-o0", "python",
+             "client.py", str(port), str(cong_ctrl)],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE)
 
     return process
 
@@ -202,33 +212,26 @@ def main():
     _generate_100mpbs_trace(Flags.parsed_args[Flags.TIME], "100Mbps.up")
     _generate_100mpbs_trace(Flags.parsed_args[Flags.TIME], "100Mbps.down")
 
-    # queues for storing output lines
-    # server_q = Queue.Queue()
-    # experiment_q = Queue.Queue()
-
-    loss_rates = Flags.parsed_args[Flags.LOSS]
     port = Flags.parsed_args[Flags.PORT]
+    loss = Flags.parsed_args[Flags.LOSS]
+    cc = Flags.parsed_args[Flags.CC]
+    for cong_ctrl in ['bbr', cc]:
+        # Start the server
+        server_proc = _start_server(port, loss, cong_ctrl)
+        client_proc = _run_experiment(loss, port, cong_ctrl)
 
-    loss_rates = Flags.parsed_args[Flags.LOSS]
-    for cong_ctrl in ['cubic', 'bbr']:
-        for loss_percent in loss_rates:
-            # Start the server
-            loss = loss_percent / 100.0
-            server_proc = _start_server(port, loss, cong_ctrl)
-            client_proc = _run_experiment(loss, port, cong_ctrl)
-
-            while True:
-                client_proc.poll()
-                if client_proc.returncode is not None:
-                    time.sleep(2)  # Give the server time to log the results.
-                    server_proc.terminate()
-                    break
+        while True:
+            client_proc.poll()
+            if client_proc.returncode is not None:
+                time.sleep(1.5)  # Give the server time to log the results.
+                server_proc.terminate()
+                break
 
     debug_print("Experiment complete! Data stored in: " + str(logfile))
     debug_print("Terminating driver.")
 
     # TODO(luke) Make the graphs from that CSV
-    _make_plots(logfile)
+    # _make_plots(logfile)
 
 
 if __name__ == '__main__':
