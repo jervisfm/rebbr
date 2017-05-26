ef3aae06beed57dda794631c74943f60a43a7fbe
Jervis <jervismuindi@yahoo.co.uk>
2017-05-26 00:14:23 -0700

    Include congestion control in experiment 1

--- mahimahi/run_experiment1.sh
+++ mahimahi/run_experiment1.sh
@@ -6,6 +6,7 @@ set +x
 
 LOSS_RATES="0.001 0.01 0.1 1 2 5 10 15 20 25 30 40 50"
 BW_MBPS="0.01 0.1 1 10 100"
+CONGESTION_CONTROL="cubic bbr"
 LOG_FILE=experiment1.csv
 
 # Clear any existing data.
@@ -14,10 +15,12 @@ rm -f $LOG_FILE
 # Run experiment.
 echo "Running  experiment 1: effect of bandwidth"
 
-for loss_rate in $LOSS_RATES; do
-  for bw in $BW_MBPS; do
-    echo "Executing trial with Loss rate: $loss_rate Bandwidth: $bw ..."
-    ./bbr_experiment.py --loss=$loss_rate --bw=$bw >> $LOG_FILE
+for cc in $CONGESTION_CONTROL; do
+  for loss_rate in $LOSS_RATES; do
+    for bw in $BW_MBPS; do
+      echo "Executing trial with cc=$cc Loss rate: $loss_rate Bandwidth: $bw ..."
+      ./bbr_experiment.py --cc=$cc --loss=$loss_rate --bw=$bw >> $LOG_FILE
+    done
   done
 done
 
