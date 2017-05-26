c1b30c4564c40d9c158668da23601fc67344d571
Jervis <jervismuindi@yahoo.co.uk>
2017-05-26 08:41:08 -0700

    Add script to help with initializing congestion control

---
+++ mahimahi/initialize_congestion_control.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set +x
+# This script is to initialize the varoius congestion controls
+# algorithms once so that they are available for use.
+
+echo "We need sudo so you'll be prompted for your password"
+
+CONGESTION_CONTROL="bbr bic vegas westwood reno cubic"
+
+
+for cc in $CONGESTION_CONTROL; do    
+  echo "Initializing $cc"
+  sudo sysctl -w net.ipv4.tcp_congestion_control=$cc
+done
+
+echo "Initialization complete"
