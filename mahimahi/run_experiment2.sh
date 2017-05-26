af787da5b58d4c8b2dd8b7ff95d86c4044e63c22
Jervis <jervismuindi@yahoo.co.uk>
2017-05-26 00:20:50 -0700

    Add script to look at effect of congestion control algorithms

--- mahimahi/run_experiment2.sh
+++ mahimahi/run_experiment2.sh
@@ -4,4 +4,18 @@ set +x
 # This script simple runs an experiment for looking at
 # different congestion control algorithms.
 
-# TODO: implement.
+LOSS_RATES="0.001 0.01 0.1 1 2 5 10 15 20 25 30 40 50"
+CONGESTION_CONTROL="cubic bbr bic vegas westwood reno"
+LOG_FILE=experiment2.csv
+
+# Clear any existing data.
+rm -f $LOG_FILE
+
+# Run experiment.
+echo "Running experiment 2: Effect of different Congestion Control Algorithms."
+for cc in $CONGESTION_CONTROL; do
+  for loss_rate in $LOSS_RATES; do
+    echo "Executing trial with cc=$cc Loss rate: $loss_rate ..."
+    ./bbr_experiment.py --cc=$cc --loss=$loss_rate >> $LOG_FILE
+  done
+done
