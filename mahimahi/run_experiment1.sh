a2cb38099454677753d48be72d8d3ca625d79016
Jervis <jervismuindi@yahoo.co.uk>
2017-05-25 23:56:44 -0700

    Fix a comma bash syntax error

--- mahimahi/run_experiment1.sh
+++ mahimahi/run_experiment1.sh
@@ -5,7 +5,7 @@ set +x
 # various bandwidth between CUBIC and BBR.
 
 
-LOSS_RATES="0.001, 0.01, 0.1, 1, 2, 5, 10, 15, 20, 25, 30, 40, 50"
+LOSS_RATES="0.001 0.01 0.1 1 2 5 10 15 20 25 30 40 50"
 LOG_FILE=experiment1.csv
 
 # Clear any existing data.
