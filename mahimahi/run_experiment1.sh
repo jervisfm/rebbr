84d8145fa217fb280393eb6e145ecb5c97e1baac
Jervis <jervismuindi@yahoo.co.uk>
2017-05-26 00:00:32 -0700

    WIP figure 8 experiment script

--- mahimahi/run_experiment1.sh
+++ mahimahi/run_experiment1.sh
@@ -5,15 +5,4 @@ set +x
 # various bandwidth between CUBIC and BBR.
 
 
-LOSS_RATES="0.001 0.01 0.1 1 2 5 10 15 20 25 30 40 50"
-LOG_FILE=experiment1.csv
-
-# Clear any existing data.
-rm -f $LOG_FILE
-
-# Run experiment.
-for loss_rate in $LOSS_RATES; do
-  echo "Executing trial with Loss rate: $loss_rate ..."
-  ./bbr_experiment.py --loss=$loss_rate >> $LOG_FILE
-done
 
--- mahimahi/run_figure8_experiment.sh
+++ mahimahi/run_figure8_experiment.sh
@@ -4,4 +4,14 @@ set +x
 # This script simple runs experiment to reproduce figure 8
 # in the original paper.
 
-# TODO: implement.
+LOSS_RATES="0.001 0.01 0.1 1 2 5 10 15 20 25 30 40 50"
+LOG_FILE=figure8_experiment.csv
+
+# Clear any existing data.
+rm -f $LOG_FILE
+
+# Run experiment.
+for loss_rate in $LOSS_RATES; do
+  echo "Executing trial with Loss rate: $loss_rate ..."
+  ./bbr_experiment.py --loss=$loss_rate >> $LOG_FILE
+done
