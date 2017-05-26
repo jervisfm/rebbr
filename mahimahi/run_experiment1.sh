2753eb5fc9b2f48fe1cb55bec081c30e7c8340ee
Jervis <jervismuindi@yahoo.co.uk>
2017-05-25 23:51:03 -0700

    WIP on experiment 1

--- mahimahi/run_experiment1.sh
+++ mahimahi/run_experiment1.sh
@@ -2,6 +2,17 @@
 
 set +x
 # This script simple runs an experiment for looking at effect of
-# various bandwidth.
+# various bandwidth between CUBIC and BBR.
 
-# TODO: implement.
+
+LOSS_RATES="0.001, 0.01, 0.1, 1, 2, 5, 10, 15, 20, 25, 30, 40, 50"
+LOG_FILE=experiment1.csv
+
+# Clear any existing data.
+rm -f $LOG_FILE
+
+# Run experiment.
+for loss_rate in $LOSS_RATES; do
+  ./bbr_experiment.py --loss=$loss_rate >> $LOG_FILE
+done
+
