bc866f246261ed90cc6870afa64f31b001ba76ca
Jervis <jervismuindi@yahoo.co.uk>
2017-05-27 13:22:50 -0700

    For figure 8 experiment, stop running the shell script if any of the executions fail

--- mahimahi/run_figure8_experiment.sh
+++ mahimahi/run_figure8_experiment.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-set +x
 # This script simple runs experiment to reproduce figure 8
 # in the original paper.
+
+set -x # Enable logging of executed commands.
+set -e # Stop if any error occurs.
 
 LOSS_RATES="0.001 0.01 0.1 1 2 5 10 15 20 25 30 40 50"
 CONGESTION_CONTROL="cubic bbr"
