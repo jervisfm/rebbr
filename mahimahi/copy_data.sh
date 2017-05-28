f3bd69d49eed08b97d8b7bc67d5fdada21f03733
Jervis <jervismuindi@yahoo.co.uk>
2017-05-28 12:08:10 -0700

    Fix typo in copy data script

--- mahimahi/copy_data.sh
+++ mahimahi/copy_data.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-set +x
 # This script simple copys experimental results into the data folder.
+set -e
+set -x
 
 FILES="figure8_experiment.csv experiment1.csv experiment2.csv experiment3.csv"
 
-for $file in FILES; do
+for file in FILES; do
   echo "Copying $file..."
   cp -f $file data/$file
 done
