10c9b957c2f840a0011008fdd2a1cc95e247d46d
Jervis <jervismuindi@yahoo.co.uk>
2017-05-28 12:08:46 -0700

    Fix another typo in copy data script

--- mahimahi/copy_data.sh
+++ mahimahi/copy_data.sh
@@ -6,7 +6,7 @@ set -x
 
 FILES="figure8_experiment.csv experiment1.csv experiment2.csv experiment3.csv"
 
-for file in FILES; do
+for file in $FILES; do
   echo "Copying $file..."
   cp -f $file data/$file
 done
