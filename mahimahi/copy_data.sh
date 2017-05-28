c48c2e9045ab2c767f539756896f79e69204f971
Jervis <jervismuindi@yahoo.co.uk>
2017-05-28 12:06:27 -0700

    Fix copy data script

--- mahimahi/copy_data.sh
+++ mahimahi/copy_data.sh
@@ -8,6 +8,6 @@ FILES="figure8_experiment.csv experiment1.csv experiment2.csv experiment3.csv"
 for $file in FILES; do
   echo "Copying $file..."
   cp -f $file data/$file
+done
 
 echo "Data copy complete"
-
