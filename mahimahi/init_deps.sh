406a4e6c6facca64bec22ea754f27ac742bea95c
Jervis <jervismuindi@yahoo.co.uk>
2017-05-31 04:23:55 +0000

    Update initialize script to include other needed dependencies, mahimahi etc

--- mahimahi/init_deps.sh
+++ mahimahi/init_deps.sh
@@ -6,5 +6,16 @@ set -e # Stop if any error occurs.
 
 # This script simple installs needed depends on GCloud VM.
 
+echo "Installing Python PIP"
 sudo apt-get install -y python-pip
+
+echo "Installing Python TK for graph plotting"
 sudo apt-get install -y python-tk
+pip install matplotlib
+
+
+# Install Mahimahi
+echo "Installing Mahimahi"
+sudo add-apt-repository -y ppa:keithw/mahimahi
+sudo apt-get update
+sudo apt-get install -y mahimahi
