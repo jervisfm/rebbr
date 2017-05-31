b66d6bb86a243258eb013ef402ad1419730637e3
Jervis <jervismuindi@yahoo.co.uk>
2017-05-31 04:18:33 +0000

    Add script to initialize needed dependencies

---
+++ mahimahi/init_deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+set -x # Enable logging of executed commands.
+set -e # Stop if any error occurs.
+
+# This script simple installs needed depends on GCloud VM.
+
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-tk
