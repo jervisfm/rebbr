b0f1cec6c63eede7b4c05f60254a7b9f2b91cd48
Jervis <jervismuindi@yahoo.co.uk>
2017-05-30 20:29:18 -0700

    Update copy figure script to Stop on error

--- mahimahi/copy_figures.sh
+++ mahimahi/copy_figures.sh
@@ -2,7 +2,8 @@
 
 
 # This script simple copys experimental results into the data folder.
-set -x
+set -x # Log executed commands
+set -e # Stop on error.
 
 FILES="figure8.png experiment1_figure.png experiment2_figure.png experiment3_figure.png"
 
