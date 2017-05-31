e6bbcb7dcbde357aa273ce30dd8942a59fd920bb
Jervis <jervismuindi@yahoo.co.uk>
2017-05-30 20:45:52 -0700

    Include graph plotting services after doing data generation

--- mahimahi/run_experiments.sh
+++ mahimahi/run_experiments.sh
@@ -5,8 +5,12 @@ set -e # Stop if any error occurs.
 
 # This script simple runs a handful of experiments.
 
+# Generate all the needed data.
 ./run_figure8_experiment.sh $@
 ./run_experiment1.sh $@
 ./run_experiment2.sh $@
 ./run_experiment3.sh $@
 ./run_experiment4.sh $@
+
+# Plot the results.
+./bbr_plot.py
