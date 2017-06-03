ff43feccc8f5ab8ed623716aaa5568ab4609929d
Luke Hsiao <lwhsiao@stanford.edu>
2017-06-03 01:20:04 -0700

    Add printout of start and end time.

--- run_all.sh
+++ run_all.sh
@@ -7,5 +7,7 @@ cd mahimahi
 # First, install all dependencies
 ./init_deps.sh
 
+echo $(date)
 # This script simply runs all experiments with headless mode enabled.
 ./run_experiments_headless.sh
+echo $(date)
