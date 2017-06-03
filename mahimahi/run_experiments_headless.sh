d8b5ca2640bd3de81aab34e72305a09b40c76d6a
Luke Hsiao <lwhsiao@stanford.edu>
2017-06-02 21:09:40 -0700

    Add HTTPServer to allow them to browse the figures.

--- mahimahi/run_experiments_headless.sh
+++ mahimahi/run_experiments_headless.sh
@@ -6,3 +6,7 @@ set -e # Stop if any error occurs.
 # This script simple runs all experiments with headless mode enabled.
 
 ./run_experiments.sh --headless
+
+# Then, serve the content on http
+echo "Open a browser and navigate to http://<ip_address>/bbr/mahimahi/data/ to view the figures."
+sudo python -m SimpleHTTPServer 80
