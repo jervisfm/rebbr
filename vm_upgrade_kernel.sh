f95cc0e72c62feae408fe5218f00d37d71523427
Luke Hsiao <lwhsiao@stanford.edu>
2017-06-02 20:52:43 -0700

    Remove the debs after the kernel has been installed.

--- vm_upgrade_kernel.sh
+++ vm_upgrade_kernel.sh
@@ -10,4 +10,5 @@ wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.11.1/linux-image-4.11.1-04
 echo "Installing new kernel. Will be prompted to enter password"
 sudo dpkg -i *.deb
 
+rm -f *.deb
 echo "Reboot the Machine and verify installation with uname -sr"
