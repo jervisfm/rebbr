32d42116fddbb1bf861284fbea060c096b5a414a
Luke Hsiao <lwhsiao@stanford.edu>
2017-06-03 12:06:46 -0700

    Use lower RTTs for experiment 3

--- mahimahi/run_experiment3.sh
+++ mahimahi/run_experiment3.sh
@@ -9,7 +9,7 @@ mkdir -p data
 
 LOSS_RATES="0.001 0.01 0.1 1 2 5 10 15 20 25 30 40 50"
 # Mahimahi min supported RTT is 2ms
-RTTS_MS="2 10 100 1000 2000"
+RTTS_MS="2 10 100 200 500"
 CONGESTION_CONTROL="cubic bbr"
 LOG_FILE=data/experiment3.csv
 
@@ -23,7 +23,7 @@ for cc in $CONGESTION_CONTROL; do
   for loss_rate in $LOSS_RATES; do
     for rtt in $RTTS_MS; do
       echo "Executing trial with cc=$cc Loss rate: $loss_rate RTT (ms): $rtt ..."
-      ./bbr_experiment.py --cc=$cc --loss=$loss_rate --rtt=$rtt --output_file=$LOG_FILE $@
+      ./bbr_experiment.py --cc=$cc --loss=$loss_rate --time 120 --rtt=$rtt --output_file=$LOG_FILE $@
     done
   done
 done
--- mahimahi/run_experiments.sh
+++ mahimahi/run_experiments.sh
@@ -4,7 +4,6 @@ set -x # Enable logging of executed commands.
 set -e # Stop if any error occurs.
 
 # This script simple runs a handful of experiments.
-
 ./initialize_congestion_control.sh
 
 # Generate all the needed data.
